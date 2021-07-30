0d8b7ceb530218655bfd4ef2c6a7d7bcb2b0b036
Blunkx <asd9521889@gmail.com>
2021-07-30 12:00:29 +0800

    fix bug, remove wrong suite tests

--- test.py
+++ test.py
@@ -40,6 +40,7 @@ def create_func_dict(fun, suites_csv, status_dict):
     temp = read_json(uid_name)
     for row in suites_csv:
         if fun['name'] == row[9]:
+            row[4] = '' if row[4] == 'tests' else row[4]
             suite = row[4].replace('.', '/')
             file_name = suite+'/'+row[5] + \
                 '.py' if suite != '' else row[5]+'.py'
@@ -62,9 +63,19 @@ def create_func_dict(fun, suites_csv, status_dict):
 
 
 def merger_para_func(func_set, status_dict, func_list):
-    new_list = list()
-    for func in func_set:
-        print(status_dict[func])
+    new_list = func_list = list(
+        {v['func_name']: v for v in func_list}.values())
+    print(len(new_list))
+    #{'suite': suite, 'file_name': file_name, 'topo_marker': topo_marker,'func_name': path, 'status': fun["status"], 'UID': fun["uid"]}
+    for row in new_list:
+        print(status_dict[row['func_name']])
+        if status_dict[row['func_name']].count('broken') > 0 or status_dict[row['func_name']].count('failed') > 0:
+            row['status'] = 'failed'
+        elif status_dict[row['func_name']].count('passed') > 0:
+            row['status'] = 'passed'
+        elif status_dict[row['func_name']].count('skipped') == len(status_dict[row['func_name']]):
+            row['status'] = 'skipped'
+    write_output('output.csv', new_list)
 
 
 def write_output(file_name, func_list):
@@ -88,5 +99,5 @@ func_set = sorted(func_set)
 func_list = sorted(func_list, key=lambda k: k['func_name'])
 func_list = sorted(func_list, key=lambda k: k['suite'])
 func_list = sorted(func_list, key=lambda k: k['file_name'])
-#merger_para_func(func_set, status_dict, func_list)
-write_output('output.csv', func_list)
+merger_para_func(func_set, status_dict, func_list)
+#write_output('output.csv', func_list)
