b226e47e4cba0e7213d1eb796224feb6f0ddc6b6
blunkx <asd9521889@gmail.com>
2022-03-06 19:33:59 +0800

    follow the google coding style and create output funtion

--- SDK.py
+++ SDK.py
@@ -1,8 +1,8 @@
-import os
-import json
-import re
+#!/usr/bin/env python3
 import csv
+import json
 import sys
+from pathlib import Path
 
 
 def read_csv(file_name):
@@ -13,8 +13,7 @@ def read_csv(file_name):
     Returns:
         list(csv.reader(input_file, delimiter=","))(list): two dimentional list from csv
     """
-    csv_path = os.getcwd() + "/allure-report/data/" + file_name
-    # csv_path = file_name
+    csv_path = Path.cwd() / "allure-report" / "data" / file_name
     try:
         with open(csv_path) as input_file:
             return list(csv.reader(input_file, delimiter=","))
@@ -23,8 +22,38 @@ def read_csv(file_name):
         sys.exit()
 
 
+def write_csv(file_name, function_list):
+    """
+    If there is no file_path, print all data.
+    Otherwise output the data to CSV files.
+    Args:
+        file_name(str): filename to output
+        function_list(list): list of all functions, each stored as a dict
+    Returns:
+        None
+    """
+    file_path = Path.cwd() / file_name
+    try:
+        with open(file_path, "w", newline="") as out:
+            row = list(function_list[0].keys())
+            csv.writer(out).writerow(row)
+            for func in function_list:
+                row = list(func.values())
+                csv.writer(out).writerow(row)
+    except Exception:
+        print("Failed to open!")
+        sys.exit()
+
+
 def get_case_num():
-    csv_path = os.getcwd() + "/allure-report/history/retry-trend.json"
+    """
+    Get allure report number.
+    Args:
+        None
+    Returns:
+        data[0]["buildOrder"](int): the report number
+    """
+    csv_path = Path.cwd() / "allure-report" / "history" / "retry-trend.json"
     try:
         with open(csv_path) as input_file:
             data = json.load(input_file)
@@ -52,7 +81,7 @@ def sort_func_list(func_list):
 def create_func_list(suite_csv):
     """
     Create the sorted list of all functions and two dicts to
-    Args: 
+    Args:
         suites_csv(list): two dimentinal list read from suite.csv
     Returns:
         result(list): list of all functions, each function is stored in a dict
@@ -63,16 +92,11 @@ def create_func_list(suite_csv):
         file_name = row[4] + "/" + row[5] + ".py"
         topo_marker = ""
         func_name = file_name + "::" + row[9]
-        # Status	Start Time	Stop Time	Duration in ms	Parent Suite	Suite	Sub Suite	Test Class	Test Method    Name    Description
-        # 0         1           2           3               4               5       6           7           8              9       10
-
-        # suite = parent_suite
-        # file_name = parent_suite + suite + .py
-        # topo_marker = ?
-        # func_name = file_name + :: + name
-        # status
+        """
+        Status	Start Time	Stop Time	Duration in ms	Parent Suite	Suite	Sub Suite	Test Class	Test Method    Name    Description
+        0       1           2           3               4               5       6           7           8              9       10
+        """
         match = "x" if row[5] not in row[9] else ""
-        #print(row[5], "", row[9])
         func_dict = {
             "suite": suite,
             "file_name": file_name,
@@ -85,14 +109,12 @@ def create_func_list(suite_csv):
     return result
 
 
-suite_csv = read_csv("suites.csv")
-suite_csv.pop(0)
-result = create_func_list(suite_csv)
-result = sort_func_list(result)
-file_path = "sdk_tests #{}.csv".format(get_case_num())
-with open(file_path, "w", newline="") as out:
-    row = list(result[0].keys())
-    csv.writer(out).writerow(row)
-    for func in result:
-        row = list(func.values())
-        csv.writer(out).writerow(row)
+def main():
+    suite_csv = read_csv("suites.csv")
+    suite_csv.pop(0)
+    result = sort_func_list(create_func_list(suite_csv))
+    write_csv("sdk_tests #{}.csv".format(get_case_num()), result)
+
+
+if __name__ == "__main__":
+    main()
